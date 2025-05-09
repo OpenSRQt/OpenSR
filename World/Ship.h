2bbf1c320e396ae96bb3cf0a4427b013b4e7b56a
InfernalKn1ght <arzhaev.dmitry@yandex.ru>
2025-05-09 12:37:22 +0300

    refactor: remove ship arrival check on timer

--- World/Ship.h
+++ World/Ship.h
@@ -162,6 +162,7 @@ private:
     float m_targetAngle;
     QPointF m_destination;
     QPointF m_start_position;
+    bool m_isMoving = false;
 
     bool m_isNearPlanet = false;
     bool m_isMoving = false;
